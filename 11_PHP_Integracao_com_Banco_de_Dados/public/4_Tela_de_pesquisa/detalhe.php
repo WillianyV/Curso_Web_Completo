2f5a06127b49f120b235b4238e271fefb082b294
WillianyV <41123970+WillianyV@users.noreply.github.com>
2021-07-28 16:15:04 -0300

    Finalizando modulo 6
    pesquisa

---
+++ 11_PHP_Integracao_com_Banco_de_Dados/public/4_Tela_de_pesquisa/detalhe.php
@@ -0,0 +1,68 @@
+<?php require_once("../../conexao/connection.php"); ?>
+<?php
+    
+    if(isset($_GET["codigo"])){
+        $produtoID = $_GET["codigo"];
+    }else{
+        Header("location: index.php");
+    }
+    
+    $_query = "SELECT * FROM produtos WHERE produtoID = {$produtoID} ";
+    
+    $_result = mysqli_query($_conexao,$_query);
+
+    if(!$_result){
+        die("Falha ao consultar o Banco de Dados");
+    }else{
+        $_dados_detalhes = mysqli_fetch_assoc($_result);
+
+        $_produtoID = $_dados_detalhes["produtoID"];
+        $_nome_produto = $_dados_detalhes["nomeproduto"];
+        $_descricao = $_dados_detalhes["descricao"];
+        $_codigo_barra = $_dados_detalhes["codigobarra"];
+        $_tempo_entrega = $_dados_detalhes["tempoentrega"];
+        $preco_revenda = $_dados_detalhes["precorevenda"];
+        $preco_unitario = $_dados_detalhes["precounitario"];
+        $_estoque = $_dados_detalhes["estoque"];
+        $_imagem_grande = $_dados_detalhes["imagemgrande"];
+    }
+?>
+<!doctype html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Curso PHP Integração com MySQL</title>
+        
+        <!-- estilo -->
+        <link href="_css/estilo.css" rel="stylesheet">
+        <link href="_css/produto_detalhe.css" rel="stylesheet">
+    </head>
+
+    <body>
+        <?php include_once("../_incluir/topo.php"); ?>
+        <?php include_once("../_incluir/funcoes.php"); ?>
+        
+        <main> 
+            <div id="detalhe_produto">
+                <ul>
+                    <li class="imagem"><img src="<?php echo $_imagem_grande?>" alt="<?php echo $_nome_produto?>"></li>
+                    <li><h2><?php echo $_nome_produto?></h2></li>
+                    <li>Descrição: <?php echo $_descricao?></li>
+                    <li>Cógido de Barra: <?php echo $_codigo_barra?></li>
+                    <li>Entrega: <?php echo $_tempo_entrega?> dias</li>
+                    <li>Valor de Revenda: <?php echo  real_format($preco_revenda)?></li>
+                    <li>Valor de Venda: <?php echo  real_format($preco_unitario)?></li>
+                    <li>Estoque: <?php echo $_estoque?></li>                  
+                </ul>
+            </div>
+        </main>
+
+        <?php include_once("../_incluir/rodape.php"); ?> 
+    </body>
+</html>
+
+<?php
+    // Fechar conexao
+    mysqli_close($_conexao);
+?>
---
+++ 11_PHP_Integracao_com_Banco_de_Dados/public/4_Tela_de_pesquisa/index.php
@@ -0,0 +1,68 @@
+<?php require_once("../../conexao/connection.php"); ?>
+
+<?php
+    $_query = "SELECT produtoID, nomeproduto, tempoentrega, precounitario, imagempequena FROM produtos ";
+    if(isset($_GET["produto"])){
+        $_pesquisa = $_GET["produto"];
+        $_query .= " WHERE nomeproduto LIKE '%{$_pesquisa}%' " ;   
+    }
+    $_result = mysqli_query($_conexao,$_query);
+    if(!$_result){
+        die ("Falha na consulta");
+    }
+?>
+
+
+<!doctype html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Curso PHP Integração com MySQL</title>
+        
+        <!-- estilo -->
+        <link href="_css/estilo.css" rel="stylesheet">
+        <link href="_css/produtos.css" rel="stylesheet">
+        <link href="_css/produto_pesquisa.css" rel="stylesheet">
+    </head>
+
+    <body>
+        <?php include_once("../_incluir/topo.php"); ?>
+        <?php include_once("../_incluir/funcoes.php"); ?>
+        
+        <main> 
+            <div id="janela_pesquisa">
+                <form action="index.php" method="GET">
+                    <input type="text" name="produto" placeholder="Pesquise o produto">
+                    <input type="image" name="pesquisa" src="../_assets/botao_search.png">
+                </form>
+            </div>            
+            <div id="listagem_produtos">
+                <?php
+                    while($linha = mysqli_fetch_assoc($_result)){
+                ?> 
+                    <ul>
+                        <li class="imagem">
+                            <a href="detalhe.php?codigo=<?php echo $linha["produtoID"]?>">
+                                <img src="<?php echo $linha["imagempequena"]?>" alt="<?php echo $linha["nomeproduto"]?>">
+                            </a>
+                        </li>
+                        <li><h3><?php echo $linha["nomeproduto"];?></h3></li>
+                        <li>Entrega: <?php echo $linha["tempoentrega"];?> dias</li>
+                        <li>Valor: R$ <?php echo $linha["precounitario"];?></li>
+                    </ul>
+                <?php
+                    }
+                ?>
+            </div>
+            
+        </main>
+
+        <?php include_once("../_incluir/rodape.php"); ?> 
+    </body>
+</html>
+
+<?php
+    // Fechar conexao
+    mysqli_close($_conexao);
+?>
