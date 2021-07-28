36233cc26f18aa7fe200a93b6fe83ebbfb482173
WillianyV <41123970+WillianyV@users.noreply.github.com>
2021-07-28 11:52:02 -0300

    finalizando o modulo 2
    listagem de produtos

--- 11 - PHP Integração com Banco de Dados/_incluir/topo.php
+++
@@ -1,6 +0,0 @@
-<header>
-    <div id="header_central">
-        <img src="/11 - PHP Integração com Banco de Dados/public/_assets/logo_andes.gif">
-        <img src="/11 - PHP Integração com Banco de Dados/public/_assets/text_bnwcoffee.gif">  
-    </div>
-</header>
--- 11 - PHP Integração com Banco de Dados/public/2_Listagem_de_Produtos/index.php
+++ 11 - PHP Integração com Banco de Dados/public/2_Listagem_de_Produtos/index.php
@@ -1,5 +1,13 @@
 <?php require_once("../../conexao/connection.php"); ?>
 
+<?php
+    $_query = "SELECT produtoID, nomeproduto, tempoentrega, precounitario, imagempequena  FROM produtos ";
+    $_result = mysqli_query($_conexao,$_query);
+    if(!$_result){
+        die ("Falha na consulta");
+    }
+?>
+
 <!doctype html>
 <html>
     <head>
@@ -9,21 +17,36 @@
         
         <!-- estilo -->
         <link href="_css/estilo.css" rel="stylesheet">
+        <link href="_css/produtos.css" rel="stylesheet">
     </head>
 
     <body>
-        <?php include_once("../../_incluir/topo.php"); ?>
-        <?php include_once("../../_incluir/funcoes.php"); ?>
+        <?php include_once("../_incluir/topo.php"); ?>
+        <?php include_once("../_incluir/funcoes.php"); ?>
         
-        <main>  
+        <main> 
+            <div id="listagem_produtos">
+                <?php
+                    while($linha = mysqli_fetch_assoc($_result)){
+                ?> 
+                    <ul>
+                        <li class="imagem"><img src="<?php echo $linha["imagempequena"]?>" alt="<?php echo $linha["nomeproduto"]?>"></li>
+                        <li><h3><?php echo $linha["nomeproduto"];?></h3></li>
+                        <li>Entrega: <?php echo $linha["tempoentrega"];?> dias</li>
+                        <li>Valor: R$ <?php echo $linha["precounitario"];?></li>
+                    </ul>
+                <?php
+                    }
+                ?>
+            </div>
             
         </main>
 
-        <?php include_once("../../_incluir/rodape.php"); ?> 
+        <?php include_once("../_incluir/rodape.php"); ?> 
     </body>
 </html>
 
 <?php
     // Fechar conexao
-    mysqli_close($conecta);
+    mysqli_close($_conexao);
 ?>
