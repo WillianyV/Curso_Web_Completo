d64ed349ec222a7c0acbf8bda46c04e01a1d2a0f
WillianyV <41123970+WillianyV@users.noreply.github.com>
2021-08-18 11:44:37 -0300

    Finalizando a 5 unidade

--- PHP/3_AJAX/unidade_05/exercicio.php
+++ PHP/3_AJAX/unidade_05/exercicio.php
@@ -3,13 +3,30 @@
     <head>
         <meta charset="UTF-8">
         <title>PHP com AJAX</title>
+        <!-- <link href="_css/estilo.css" rel="stylesheet"> -->
     </head>
 
     <body>
+        <button id="botao">Carregar</button>
         <div id="listagem"></div>
         <script src="jquery.js"></script>
         <script>
-
+            $('#botao').click(function(){
+                $('#listagem').click('display','block');
+                carregarDados();
+            });
+            function carregarDados(){
+                $.getJSON('_json/produtos.json', function(data){
+                    var list;
+                    list = "<ul>"
+                    $.each(data, function(i, valor){
+                        list += "<li class='nome'>" + valor.nomeproduto + "</li>";
+                        list += "<li class='preco'>" + valor.precounitario + "</li>";
+                    });
+                    list += "</ul>";
+                    $('#listagem').html(list);
+                });
+            }
         </script>
     </body>
 </html>
