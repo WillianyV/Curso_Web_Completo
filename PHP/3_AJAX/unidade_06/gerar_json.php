7aa85d6342126c96ef686a8c400e5f964b95efc5
WillianyV <41123970+WillianyV@users.noreply.github.com>
2021-08-18 16:08:34 -0300

    finalizado unidade 06

---
+++ PHP/3_AJAX/unidade_06/gerar_json.php
@@ -0,0 +1,21 @@
+<?php
+    //config geral
+    header('Access-Control-Allow-Origin:*');
+    
+    $conecta = mysqli_connect("localhost","root","","andes");
+
+    $selecao = "SELECT nomeproduto,precounitario,imagempequena FROM produtos";
+
+    $produtos = mysqli_query($conecta,$selecao);
+
+    $retorno = array();
+
+    while($linha = mysqli_fetch_object($produtos)){
+        $retorno[] = $linha;
+    }
+
+    echo json_encode($retorno);
+
+    mysqli_close($conecta);
+
+?>
